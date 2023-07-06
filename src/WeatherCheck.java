20766cf79c3344c81f3794bc9e2f7632ff9faae3
essamcreates <mrm.hussain@hotmail.co.uk>
2023-07-06 17:22:44 +0100

    3rd commit

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -2,13 +2,17 @@ public class WeatherCheck {
 
     public static void main(String[] args) {
 
-        int temperature = 18; // Example temperature value
-        boolean currentlyRaining = true; //Example weather condition
+        int temperature = 17; // Example temperature value
+        boolean currentlyRaining = false; //Example weather condition
 
-        if (temperature > 20 && !currentlyRaining){
-            System.out.println("Wear shorts and suncreen.");
-        } else if (temperature < 20 && currentlyRaining) {
-            System.out.println("Wear a waterproof coat.");
+        if (temperature >20 && !currentlyRaining){
+            System.out.println("It is warm and not raining. Wear shorts and sunscreen.");
+        } else if (temperature <=20 && currentlyRaining) {
+            System.out.println("It is cold and raining. Wear a waterproof coat.");
+        } else if (temperature > 20 && currentlyRaining) {
+            System.out.println("It is warm but raining. Wear appropriate clothes.");
+        } else if (temperature <20 && !currentlyRaining) {
+            System.out.println("It is cold but not raining. Wear appropriate clothes.");
         } else {
             System.out.println("Wear appropriate clothing for the current weather conditions.");
         }
